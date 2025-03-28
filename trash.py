3ce8a964b68cecd5ca33ad3a2551642ed64490cb
Nurtal <nathan.foulquier.pro@gmail.com>
2025-03-28 22:43:52 +0100

    add b cell activation

--- trash.py
+++ trash.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import math
+import os
 
 class LymphocyteB:
     """Un agent simple qui se déplace sur une grille."""
@@ -10,6 +12,7 @@ class LymphocyteB:
         self.y = y
         self.grid_size = grid_size
         self.color = 'blue'
+        self.life_span = 10
 
     def move(self):
         """Déplacement aléatoire dans la grille."""
@@ -17,7 +20,7 @@ class LymphocyteB:
         self.x = np.clip(self.x + dx, 0, self.grid_size - 1)
         self.y = np.clip(self.y + dy, 0, self.grid_size - 1)
 
-    def activate():
+    def activate(self):
         self.color = 'red'
     
 
@@ -30,6 +33,7 @@ class LymphocyteT:
         self.grid_size = grid_size
         self.color = 'green'
         self.speed = 2
+        self.life_span = 10
 
     def move(self):
         """Déplacement aléatoire dans la grille."""
@@ -37,8 +41,42 @@ class LymphocyteT:
         self.x = np.clip(self.x + dx, 0, self.grid_size - 1)
         self.y = np.clip(self.y + dy, 0, self.grid_size - 1)
 
+
+
+
+def detect_interaction(b_agent_list, t_agent_list):
+    """ """
+
+    # params
+    interaction_treshold = 2
+    
+    for b_agent in b_agent_list:
+        for t_agent in t_agent_list:
+
+            # compute distance
+            dist = math.sqrt((b_agent.x - t_agent.x)**2 + (b_agent.y - t_agent.y)**2)
+
+            # activate b cell
+            if dist <= interaction_treshold:
+                b_agent.activate()
+    
+
+
+
+
+
+
+
+
+
+
+
 def run_simulation(n_steps:int):
     """Run"""
+
+    # initialisation de l'environnement
+    if not os.path.isdir("figures"):
+        os.mkdir("figures")
 
     # Initialisation de la simulation
     grid_size = 20
@@ -49,6 +87,11 @@ def run_simulation(n_steps:int):
 
     # Simulation
     for i in tqdm(range(n_steps), desc="Simulation en cours"):
+
+
+        # detect b activation
+        detect_interaction(b_agents, t_agents)
+        
         plt.figure(figsize=(5, 5))
         plt.xlim(0, grid_size)
         plt.ylim(0, grid_size)
