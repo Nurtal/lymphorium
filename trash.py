a02d7b5fee9fe9715f97e82de067e65814284aed
Nurtal <nathan.foulquier.pro@gmail.com>
2025-04-02 15:16:39 +0200

    compute and save metrics close #4

--- trash.py
+++ trash.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import math
 import os
 import random
+import pandas as pd
 
 class LymphocyteB:
     """Un agent simple qui se déplace sur une grille."""
@@ -15,6 +16,7 @@ class LymphocyteB:
         self.color = 'blue'
         self.life_span = 10
         self.age = 0
+        self.activated = False
 
     def move(self):
         """Déplacement aléatoire dans la grille."""
@@ -24,6 +26,7 @@ class LymphocyteB:
 
     def activate(self):
         self.color = 'red'
+        self.activated = True
     
     def get_older(self):
         self.age +=1
@@ -180,12 +183,16 @@ def init_random_age(b_agents, t_agents):
     return b_pop, t_pop
 
 
-def run_simulation(n_steps:int):
+def run_simulation(n_steps:int, output_folder:str):
     """Run"""
 
     # initialisation de l'environnement
-    if not os.path.isdir("figures"):
-        os.mkdir("figures")
+    if not os.path.isdir(f"{output_folder}"):
+        os.mkdir(f"{output_folder}") 
+    if not os.path.isdir(f"{output_folder}/figures"):
+        os.mkdir(f"{output_folder}/figures")
+    if not os.path.isdir(f"{output_folder}/logs"):
+        os.mkdir(f"{output_folder}/logs")
 
     # Initialisation de la simulation
     grid_size = 20
@@ -194,11 +201,21 @@ def run_simulation(n_steps:int):
     n_t_agents = 10
     t_agents = [LymphocyteT(np.random.randint(0, grid_size), np.random.randint(0, grid_size), grid_size) for _ in range(n_t_agents)]
 
+    # init metrics
+    step_to_nb = [{"STEP":0, "VALUE":n_b_agents}]
+    step_to_nt = [{"STEP":0, "VALUE":n_t_agents}]
+    step_to_nab = [{"STEP":0, "VALUE":0}]
+    step_to_n_total = [{"STEP":0,"VALUE":n_b_agents+n_t_agents}]
+    step_to_density = [{"STEP":0, "VALUE":float(n_b_agents+n_t_agents) / (grid_size*grid_size)}]
+
     # init random age for cells
     b_agents, t_agents = init_random_age(b_agents, t_agents)
 
     # Simulation
     for i in tqdm(range(n_steps), desc="Simulation en cours"):
+
+        # init cmpts
+        n_activated_b = 0
 
         # detect b activation
         detect_interaction(b_agents, t_agents)
@@ -216,14 +233,39 @@ def run_simulation(n_steps:int):
             b_agent.move()
             b_agent.get_older()
             plt.scatter(b_agent.x, b_agent.y, c=b_agent.color)
+
+            # compute nb of activared b cells
+            if b_agent.activated:
+                n_activated_b +=1
+            
         for t_agent in t_agents:
             t_agent.move()
             t_agent.get_older()
             plt.scatter(t_agent.x, t_agent.y, c=t_agent.color)
-        plt.savefig(f"figures/step_{i}.png")
+        plt.savefig(f"{output_folder}/figures/step_{i}.png")
         plt.close()
 
+        # update metrics
+        step_to_nb.append({"STEP":i, "VALUE":len(b_agents)})
+        step_to_nt.append({"STEP":i, "VALUE":len(t_agents)})
+        step_to_nab.append({"STEP":i, "VALUE":n_activated_b})
+        step_to_n_total.append({"STEP":i, "VALUE":len(b_agents)+len(t_agents)})
+        step_to_density.append({"STEP":i, "VALUE":float(len(b_agents)+len(t_agents)) / (grid_size*grid_size)})
+
+
+    # save metrics in logs
+    df = pd.DataFrame(step_to_nb)
+    df.to_csv(f"{output_folder}/logs/nb_bcell.csv", index=False)
+    df = pd.DataFrame(step_to_nt)
+    df.to_csv(f"{output_folder}/logs/nb_tcell.csv", index=False)
+    df = pd.DataFrame(step_to_nab)
+    df.to_csv(f"{output_folder}/logs/nb_activated_bcell.csv", index=False)
+    df = pd.DataFrame(step_to_n_total)
+    df.to_csv(f"{output_folder}/logs/nb_total.csv", index=False)
+    df = pd.DataFrame(step_to_density)
+    df.to_csv(f"{output_folder}/logs/density.csv", index=False)
+    
 
 if __name__ == "__main__":
 
-    run_simulation(100)
+    run_simulation(100, "/tmp/zog")
