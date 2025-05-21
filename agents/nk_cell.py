8b22f99b6bdc9408b3cda0d0e1e40cfde1f3bf6b
Nurtal <nathan.foulquier.pro@gmail.com>
2025-05-21 22:31:17 +0200

    create agent #23

---
+++ agents/nk_cell.py
@@ -0,0 +1,29 @@
+import numpy as np
+import random
+
+class NaturalKiller:
+    """Un agent simple qui se déplace sur une grille."""
+
+    def __init__(self, x, y, grid_size):
+        self.x = x
+        self.y = y
+        self.grid_size = grid_size
+        self.color = 'purple'
+        self.life_span = 10
+        self.age = 0
+        self.activated = False
+
+    def move(self):
+        """Déplacement aléatoire dans la grille."""
+        dx, dy = np.random.choice([-1, 0, 1]), np.random.choice([-1, 0, 1])
+        self.x = np.clip(self.x + dx, 0, self.grid_size - 1)
+        self.y = np.clip(self.y + dy, 0, self.grid_size - 1)
+
+    def get_older(self):
+        self.age +=1
+
+    def cell_division(self):
+        new_cell = NaturalKiller(self.x+1, self.y, self.grid_size)
+        new_cell.color = self.color
+        new_cell.age = 0
+        return new_cell
